eb20c4e302eefc3dc611f5034ba28c39f7b27d5f
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-02-15 23:25:50 -0800

    finished horoscope

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Lucas Eastburn
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,36 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string first;
+string last;
+int luck;
+int love;
   //get user input
+cout<<"What is your first name?"<<endl;
+cin>>first;
+cout<<"What is your last name?"<<endl;
+cin>>last;
+cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl;
 
   //tell fortune
+luck=first.length();
+love=last.length()-1;
+cout<<"your lucky number is "<<luck<<endl;
 
-  return 0;
+if(first[0]='A'||'a'||'E'||'e'||'I'||'i'||'O'||'o'||'U'||'u'){
+cout<<"you are destined to be famous!"<<endl;
+
+}else{
+cout<<"you should keep a low profile."<<endl;
+}
+
+if(last[love]='A'||'a'||'E'||'e'||'I'||'i'||'O'||'o'||'U'||'u'){
+cout<<"you have already met your true love."<<endl;
+}
+
+cout<<"have a good day!"<<endl;
+
+
+
+return 0;
 }
